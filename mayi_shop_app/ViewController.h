c526fd41aa61069ac8471e5a4ad6440d825bdcb7
Lu Wei Jian <374265237@qq.com>
2018-01-24 22:24:12 +0800

    Initial Commit

---
+++ mayi_shop_app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mayi_shop_app
+//
+//  Created by JayJay on 2018/1/24.
+//  Copyright © 2018年 JayJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mayi_shop_app/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  mayi_shop_app
+//
+//  Created by JayJay on 2018/1/24.
+//  Copyright © 2018年 JayJay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
